fa3b189714c18934054f114adb0b97b1609e6e57
Connor Cahill <connorcahill913@gmail.com>
2019-01-27 21:07:02 -0800

    finished rna_transcription problem

--- go/rna-transcription/connor_trancscription.go
+++
@@ -1,5 +0,0 @@
-package strand
-
-func main() {
-	ToRNA("G")
-}
--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,25 +1,19 @@
 package strand
 
-import (
-	"fmt"
-	"strconv"
-)
-
 //ToRNA takes a strand of DNA as a string and returns the RNA match
 func ToRNA(dna string) string {
 	var rnaStrand string
 	for _, l := range dna {
-		switch strconv.QuoteRune(l) {
-		case "G":
-			rnaStrand = rnaStrand + "C"
-		case "C":
-			rnaStrand = rnaStrand + "G"
-		case "T":
-			rnaStrand = rnaStrand + "A"
-		case "A":
-			rnaStrand = rnaStrand + "U"
+		switch l {
+		case 'G':
+			rnaStrand += "C"
+		case 'C':
+			rnaStrand += "G"
+		case 'T':
+			rnaStrand += "A"
+		case 'A':
+			rnaStrand += "U"
 		}
 	}
-	fmt.Printf("rnaStrand: %s", rnaStrand)
 	return rnaStrand
 }
