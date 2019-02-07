583717095ff2595b1bd387ae349f7feff3793cc7
Connor Cahill <connorcahill913@gmail.com>
2019-02-07 12:54:28 -0800

    finished difference-of-squares problem

--- go/difference-of-squares/difference-of-squares.go
+++ go/difference-of-squares/difference-of-squares.go
@@ -1,24 +1,27 @@
 package diffsquares
 
-//SumOfSquare is the sum of squares in an array
-func SumOfSquare(numbers []int) int {
+//SumOfSquares is the sum of squares of length numbers
+func SumOfSquares(length int) int {
 	var ret int
-	for _, n := range numbers {
-		ret += n * n
+	for i := 0; i <= length; i++ {
+		ret += i * i
 	}
 	return ret
 }
 
 //SquareOfSum sums up numbers in slice and then sqaures them
-func SquareOfSum(numbers []int) int {
+func SquareOfSum(length int) int {
 	var total int
-	for _, n := range numbers {
-		total += n
+	for i := 0; i <= length; i++ {
+		total += i
 	}
 	return total * total
 }
 
 //Difference gives the difference of squares
-func Difference(numOne int, numTwo int) int {
-	return numOne - numTwo
+func Difference(length int) int {
+	numOne := SumOfSquares(length)
+	numTwo := SquareOfSum(length)
+
+	return numTwo - numOne
 }
